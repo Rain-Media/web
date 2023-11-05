0508fb7a16da8f3c1a8b3ae3496f883528a4ad89
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-11-05 09:58:51 +0300

    hamburger added for mobile navigation

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -1,18 +1,23 @@
+"use client"
+
 import React from 'react';
 import Image from "next/image";
 import logo from "../../assets/images/oooo.png";
 import Link from "next/link";
 import {routes} from "./constant";
+import {useWindowSize} from "../../hooks/useWindowSize";
 
 Navigation.propTypes = {
 
 };
 
 function Navigation(props) {
+  const window = useWindowSize();
+  
   return (
     <header className="flex justify-between items-center p-6 bg-black">
       <Image src={logo} alt="logo" width={100} height='auto'/>
-      <div className="flex gap-x-6 pr-10">
+      <div className={`${ window.width > 680 ? 'flex' : 'hidden' } gap-x-6 pr-10`}>
         <nav>
           <div className="flex gap-x-14 box-border text-white">
             {
@@ -23,6 +28,9 @@ function Navigation(props) {
           </div>
         </nav>
         <div className="text-white font-bold">TR</div>
+      </div>
+      <div className="text-white">
+        Hamburger
       </div>
     </header>
   );
