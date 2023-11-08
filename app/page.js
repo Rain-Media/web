851f5fd11eb91d94a8ef73ba70b3f83f887fe5ee
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-11-08 06:53:50 +0300

    mobile home page done.

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import {footerLinks} from "../src/components/Footer/constant";
 import Route from "../src/components/Footer/Route";
+import Image from "next/image";
+import opera from "../src/assets/images/opera.png";
 
 export default function Page() {
   const socialLinks = footerLinks.filter(item => item.name.includes('Sosyal'));
@@ -20,6 +22,18 @@ export default function Page() {
               )
           })
         }
+      </div>
+      <div className="flex flex-col sm:flex-row sm:gap-x-20 gap-y-2">
+        <div className="p-5 sm:p-20">
+          <Image src={opera}/>
+        </div>
+        <div className="flex flex-col gap-y-8 p-5 sm:p-20 justify-center">
+          <h2 className="text-2xl sm:text-[48px] text-white">MEM U ZIN</h2>
+          <p className="text-sm sm:text-[24px] max-w-xl text-white">
+            "Epik Aşkın Dansı: Mem u Zin Operası "Tutkunun, Aşkın ve İsyanın Öyküsüne Hoş Geldiniz!"
+          </p>
+          <button className="border border-[#828282] rounded-3xl py-3 text-white w-[150px]">Daha Fazla...</button>
+        </div>
       </div>
     </div>
   )
