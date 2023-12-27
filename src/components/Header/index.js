1887cd77b1f980212334e8799ffb37ada58781bc
emresarigul <emresarigul91@gmail.com>
2023-12-27 18:16:47 +0300

    about us page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,35 +1,35 @@
-"use client"
+"use client";
 
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import Image from "next/image";
 import logo from "../../assets/images/oooo.png";
 import Navigation from "./Navigation";
 import Swiper from "./Swiper";
-import {usePathname} from "next/navigation";
-import {useSelector} from "react-redux";
+import { usePathname } from "next/navigation";
+import { useSelector } from "react-redux";
 import backOur from "../../assets/images/backour.png";
 import Background from "./background";
 
 function Header() {
   const router = usePathname();
-  const { open } = useSelector(state => state.hamburger);
-  
-  console.log(router)
-  
+  const { open } = useSelector((state) => state.hamburger);
+
+  console.log(router);
+
   useEffect(() => {
-    if(open) {
-      document.body.style.overflow = 'hidden';
+    if (open) {
+      document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = "unset";
     }
-  },[open]);
-  
+  }, [open]);
+
   return (
-    <div className="relative overflow-hidden">
-      <header className={`flex justify-between items-center w-full p-6 bg-black`}>
-        <Image src={logo} alt="logo" width={100} height='auto'/>
+    <div className="absolute top-0 overflow-hidden z-10 w-full">
+      <header className={`flex justify-between items-center w-full p-6`}>
+        <Image src={logo} alt="logo" width={100} height="auto" />
         <div className={`flex gap-x-6 pr-10`}>
-          <Navigation/>
+          <Navigation />
           <div className="text-white font-bold">TR</div>
         </div>
       </header>
